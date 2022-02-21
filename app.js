8fad491fcf91549fe8861aa76d700c8fd7def0b4
Dragoș Mateo Dămoc <56765258+dragosmatteo@users.noreply.github.com>
2022-02-21 12:45:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,39 +11,24 @@ testimonials.forEach((testimonial) => {
                 console.log("Dot1");
                 if(testimonial.classList.contains(dot)){
                     testimonial.classList.add('active');
-                    // dot.classList.add('activee');
-
                     testimonial.nextElementSibling.classList.remove('active');
                     testimonial.nextElementSibling.nextElementSibling.classList.remove('active');
-
-                    // dot.nextElementSibling.classList.remove('activee');
-                    // dot.nextElementSibling.nextElementSibling.classList.remove('activee');
                 }
             } 
             if(dot === "dot2"){
                 console.log("Dot2");
                 if(testimonial.classList.contains(dot)){
                     testimonial.classList.add('active');
-                    // dot.classList.add('activee');
-
                     testimonial.previousElementSibling.classList.remove('active');
                     testimonial.nextElementSibling.classList.remove('active');
-
-                    // dot.previousElementSibling.classList.remove('activee');
-                    // dot.nextElementSibling.classList.remove('activee');
                 }
             }
             if(dot === "dot3"){
                 console.log("Dot3");
                 if(testimonial.classList.contains(dot)){
                     testimonial.classList.add('active')
-                    // dot.classList.add('activee');
-
                     testimonial.previousElementSibling.classList.remove('active');
                     testimonial.previousElementSibling.previousElementSibling.classList.remove('active');
-
-                    // dot.previousElementSibling.classList.remove('activee');
-                    // dot.previousElementSibling.previousElementSibling.classList.remove('activee');
                 }
             }
         });
